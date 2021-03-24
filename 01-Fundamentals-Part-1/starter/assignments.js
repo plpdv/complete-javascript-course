fc7949f6c6f34c5e00f2becb0e11bf779fefee1c
Pedro Luis Puente del Valle <44312117+plpdv@users.noreply.github.com>
2021-03-23 22:58:23 -0600

    commit assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = 'Mexico'
+let continent = "America"
+let population = 1
+
+console.log("Hello, I live in " + country + " that is part of", continent, "but only " + population + " person lives here, and is me :) ")
